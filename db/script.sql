bff511f0d62ee98264395e6743c5bab5584d8538
marinamorais <marinamorais.564@gmail.com>
2024-04-24 17:08:16 -0300

    create: da tabela bruxos

---
+++ db/script.sql
@@ -0,0 +1,19 @@
+-- criação da tabela dos bruxos --
+
+ CREATE TABLE bruxos (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    idade INTEGER NOT NULL,
+    casa VARCHAR(100) NOT NULL,
+    habilidade VARCHAR(100) NOT NULL,
+    status_sangue VARCHAR(50) NOT NULL,
+    patrono VARCHAR(100)
+ );
+ -- fim da criação da tabela dos bruxos --
+
+
+ -- criação da tabela de varinhas --
+
+ 
+
+
